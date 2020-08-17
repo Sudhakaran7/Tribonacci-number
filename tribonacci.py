3b7efa9d087dbaa78c17a38f84964e62d61ad106
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-17 09:21:18 +0700

    Create tribonacci.py

---
+++ tribonacci.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def tribonacci(self, n: int) -> int:
+        dp = [0,1,1]+[0 for _ in range(3,n+1)]
+        for x in range(3,n+1):
+            dp[x] = dp[x-1]+dp[x-2]+dp[x-3]
+        return dp[n]
+
+val=Solution()
+S=int(input())
+print(val.tribonacci(S))
